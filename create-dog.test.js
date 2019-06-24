213a4519bedf71464dd5ef9acd9d2fae526a3a2f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-24 11:23:34 -0700

    created a new test using the same function, but used different syntax

---
+++ create-dog.test.js
@@ -0,0 +1,14 @@
+const createDog = require('./create-dog');
+//test suite
+describe('create dog function', () => {
+  //individual test
+  it('returns a dog object', () => {
+    const newDog = createDog('spot', 5, '20lbs');
+    expect(newDog).toEqual({
+      name: 'spot', 
+      age: 5,
+      weight: '20lbs'
+    });
+  });
+});
+  
